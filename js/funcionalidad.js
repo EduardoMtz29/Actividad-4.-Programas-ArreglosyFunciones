34a1c99b7b09afbefa1406ed75fcbbb6672b4927
EduardoMtz29 <84493548+EduardoMtz29@users.noreply.github.com>
2021-10-18 14:00:26 -0500

    Add files via upload

--- js/funcionalidad.js
+++ js/funcionalidad.js
@@ -125,3 +125,28 @@ let mostrarTarjetaN = function () {
 let mostrarTarjetaP = function () {
     window.open('Programador.html');
 }
+
+//Tarea 7
+new Vue({
+    el: '#survey',
+    data: {
+        fAnswer: '',
+        timeCoding: 0,
+        motivation: '',
+        experience: '',
+        codingLanguage: '',
+        codingLanguages: [],
+        showSummary: false,
+        nLanguages: 0,
+    },
+    methods: {
+        add: function () {
+            this.codingLanguages.push(this.codingLanguage);
+            this.codingLanguage = '';
+            this.nLanguages++;
+        },
+        confirm: function () {
+            this.showSummary = !this.showSummary;
+        }
+    }
+});
